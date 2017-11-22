dbcfb5ec082ecb299158b72d9e484b010b5c7669
Nolonwabo <nolonwabo@projectcodex.co>
2017-11-22 19:26:31 +0200

    added return button

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ var app = express();
 var bookModel = require('./model');
 var ObjectId = require("mongodb").ObjectId;
 var bodyParser = require('body-parser');
+
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', '"Origin, X-Requested-With, Content-Type, Accept"');
+  if(req.method === "OPTIONS"){
+    res.header("Access-Control-Allow-Methods","GET,PUT,POST,DELETE")
+  }
   next();
 })
 app.use(express.static('public'));
